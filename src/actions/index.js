dc2947f7e48ba27fc69d709940307387321925d6
teezcode <spaschal43@gmail.com>
2020-12-19 12:42:58 +0100

    react redux blog

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+import jsonplaceholder from '../apis/jsonplaceholder';
+import _, { get } from 'lodash';
+
+export const fetchPostsAndUsers =()=> async (dispatch,getState)=>{
+     await dispatch (fetchPosts());
+     _.chain(getState().posts)
+     .map('userId')
+     .uniq()
+     .forEach(id=>dispatch(fetchUser(id)))
+     .value();
+}
+
+export const fetchPosts = ()=>
+     async (dispatch)=> {
+        const response =await  jsonplaceholder.get('/posts');
+        dispatch({type:'FETCH_POSTS', payload:response.data })
+    };
+
+
+export const fetchUser =(id)=> async dispatch=>{
+    const response= await jsonplaceholder.get(`/users/${id}`)
+
+    dispatch({type:'FETCH_USER',payload:response.data})
+}
+
+// export const fetchUser =(id)=> dispatch=>{
+//     _fetchUser(id, dispatch);  
+//   }
+  
+//   const _fetchUser = _.memoize(async(id, dispatch)=>{
+//       const response= await jsonplaceholder.get(`/users/${id}`)
+  
+//       dispatch({type:'FETCH_USER',payload:response.data})
+//   })
